2482048043599557b2cf285d67685854447853b4
jasimkhan-ariprodesign <jasim.khan@ariprodesigns.com>
2025-05-28 11:41:20 +0530

    UserTypeSelectScreen UI finished

--- src/presentation/screens/userTypeSelect/user-type-select-screen.tsx
+++ src/presentation/screens/userTypeSelect/user-type-select-screen.tsx
@@ -1,6 +1,6 @@
 import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {SafeAreaWrapper} from '../../components';
+import {PrimaryButton, SafeAreaWrapper} from '../../components';
 import {_color, _height, _ms, _mvs} from '../../../misc';
 import {_fonts, _images} from '../../../assets';
 
@@ -10,7 +10,16 @@ const UserTypeSelectScreen = () => {
       <ScrollView contentContainerStyle={styles.contentContainerStyle}>
         <Text style={styles.title}>Hello!</Text>
 
-        <Image source={_images.userType} style={styles.imgStyle} />
+        <Image source={_images.userType} style={styles.imgStyle} resizeMode="contain" />
+
+        <View style={styles.buttonCont}>
+          <PrimaryButton title="Find a Towing Service" />
+          <PrimaryButton
+            title="Publish a Towing Service"
+            buttonStyle={styles.publishBtn}
+            textStyle={styles.publishString}
+          />
+        </View>
       </ScrollView>
     </SafeAreaWrapper>
   );
@@ -20,8 +29,9 @@ export default UserTypeSelectScreen;
 
 const styles = StyleSheet.create({
   contentContainerStyle: {
-    paddingHorizontal: _ms(20),
+    paddingHorizontal: _ms(22),
     paddingTop: _mvs(24),
+    rowGap: _mvs(40),
   },
   title: {
     color: _color.black,
@@ -30,6 +40,18 @@ const styles = StyleSheet.create({
   },
   imgStyle: {
     width: '100%',
-    height: _height * 0.3,
+    height: _height * 0.4,
+  },
+  buttonCont: {
+    gap: _mvs(12),
+  },
+  publishBtn: {
+    backgroundColor: _color.transparent,
+    borderWidth: 1,
+    borderColor: _color.primary,
+    marginHorizontal: 1,
+  },
+  publishString: {
+    color: _color.black,
   },
 });
